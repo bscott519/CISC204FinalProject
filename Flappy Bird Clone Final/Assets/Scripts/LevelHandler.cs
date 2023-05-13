d593187a14249e1c5cd59b6a4e0b6353740d3c81
StudiousKodak <Nicj609@Gmail.com>
2023-05-13 18:09:45 -0400

    added intro level

--- Flappy Bird Clone Final/Assets/Scripts/LevelHandler.cs
+++ Flappy Bird Clone Final/Assets/Scripts/LevelHandler.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelHandler : MonoBehaviour
 {
+    public Button EasyButton, MediumButton, HardButton, IntroButton;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void OpenIntroScene()
+    {
+        SceneManager.LoadScene("IntroScene");
     }
 
     public void OpenEasyScene()
